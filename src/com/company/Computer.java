62c5a1efb1d1c8a5c8049114669e7e919dcef97e
AndriyPlytka <andriyplutka@gmail.com>
2022-07-02 01:55:18 +0300

    HidenClass

---
+++ src/com/company/Computer.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Computer {
+    class Proccessor {
+        private boolean isStart = false;
+
+        public void start () {
+            isStart = true;
+            System.out.println("isStart = " + isStart);
+        }
+
+        public void shutdown () {
+            isStart = false;
+        }
+    }
+    class RAM {
+        private boolean isStart = false;
+
+        public void start () {
+            isStart = true;
+            System.out.println("RAM is start");
+        }
+
+        public void shutdown () {
+            isStart = false;
+            System.out.println("isStart = " + isStart);
+        }
+    }
+
+    Proccessor i7 = new Proccessor();
+    RAM transfer = new RAM();
+}
